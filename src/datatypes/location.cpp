KDE/kpublictransport

commit 10edfedf4ac14823967b9beddb8dac49c824dcd7

Fix build on windows
Summary:
Otherwise build fails with

```
C:\CraftRoot\build\_\f41ea8b4\kpublictransport\src\datatypes\location.cpp(184): error C2015: Zu viele Zeichen in der Konstante
C:\CraftRoot\build\_\f41ea8b4\kpublictransport\src\datatypes\location.cpp(185): error C2015: Zu viele Zeichen in der Konstante
C:\CraftRoot\build\_\f41ea8b4\kpublictransport\src\datatypes\location.cpp(186): error C2015: Zu viele Zeichen in der Konstante
C:\CraftRoot\build\_\f41ea8b4\kpublictransport\src\datatypes\location.cpp(187): error C2015: Zu viele Zeichen in der Konstante
C:\CraftRoot\build\_\f41ea8b4\kpublictransport\src\datatypes\location.cpp(188): error C2015: Zu viele Zeichen in der Konstante
```

Reviewers: vkrause, vonreth

Reviewed By: vkrause

Differential Revision: https://phabricator.kde.org/D25571

src/datatypes/location.cpp
@@ -178,14 +178,14 @@ static QString stripDiacritics(const QString &s)
 
 // keep this ordered (see https://en.wikipedia.org/wiki/List_of_Unicode_characters)
 struct {
-    ushort key;
+    QChar key;
     const char* replacement;
 } static const transliteration_map[] = {
-    { u'ß', "ss" },
-    { u'ä', "ae" },
-    { u'ö', "oe" },
-    { u'ø', "oe" },
-    { u'ü', "ue" },
+    { 'ß', "ss" },
+    { 'ä', "ae" },
+    { 'ö', "oe" },
+    { 'ø', "oe" },
+    { 'ü', "ue" },
 };
 
 static QString applyTransliterations(const QString &s)
@@ -197,7 +197,7 @@ static QString applyTransliterations(const QString &s)
         const auto it = std::lower_bound(std::begin(transliteration_map), std::end(transliteration_map), c, [](const auto &lhs, const auto rhs) {
             return QChar(lhs.key) < rhs;
         });
-        if (it != std::end(transliteration_map) && QChar((*it).key) == c) {
+        if (it != std::end(transliteration_map) && (*it).key == c) {
             res += QString::fromUtf8((*it).replacement);
             continue;
         }

commit 8ff84b4b059b53d9681878632802469e55f628d4

Fix transliteration map regression from D25571
This caused unit test failures on Linux, and broke the Android build.

src/datatypes/location.cpp
@@ -178,14 +178,14 @@ static QString stripDiacritics(const QString &s)
 
 // keep this ordered (see https://en.wikipedia.org/wiki/List_of_Unicode_characters)
 struct {
-    QChar key;
+    ushort key;
     const char* replacement;
 } static const transliteration_map[] = {
-    { 'ß', "ss" },
-    { 'ä', "ae" },
-    { 'ö', "oe" },
-    { 'ø', "oe" },
-    { 'ü', "ue" },
+    { 0x00DF, "ss" }, // ß
+    { 0x00E4, "ae" }, // ä
+    { 0x00F6, "oe" }, // ö
+    { 0x00F8, "oe" }, // ø
+    { 0x00FC, "ue" }, // ü
 };
 
 static QString applyTransliterations(const QString &s)
@@ -197,7 +197,7 @@ static QString applyTransliterations(const QString &s)
         const auto it = std::lower_bound(std::begin(transliteration_map), std::end(transliteration_map), c, [](const auto &lhs, const auto rhs) {
             return QChar(lhs.key) < rhs;
         });
-        if (it != std::end(transliteration_map) && (*it).key == c) {
+        if (it != std::end(transliteration_map) && QChar((*it).key) == c) {
             res += QString::fromUtf8((*it).replacement);
             continue;
         }