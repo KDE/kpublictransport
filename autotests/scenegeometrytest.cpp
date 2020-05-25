edf79309121a8bd85b9e41fac1a487feca9155a9
Volker Krause <vkrause@kde.org>
2020-05-25 17:15:22 +0200

    Second attempt to fix this on 32bit platforms

--- autotests/scenegeometrytest.cpp
+++ autotests/scenegeometrytest.cpp
@@ -38,7 +38,11 @@ private Q_SLOTS:
         QCOMPARE(SceneGeometry::polygonCentroid(p3), QPointF(0, 0));
 
         QPolygonF p4{{{0.273, 0.146}, {0.423, 0.496}, {0.415, 0.499}, {0.266, 0.149}}};
-        QCOMPARE((SceneGeometry::polygonCentroid(p4) * 1000000).toPoint(), QPoint(345669, 325821));
+//         QCOMPARE((SceneGeometry::polygonCentroid(p4) * 1000000).toPoint(), QPoint(345669, 325821));
+        // make the test pass on 32bit platforms too
+        QCOMPARE((SceneGeometry::polygonCentroid(p4) * 1000000).toPoint().x(), 345669);
+        QVERIFY((SceneGeometry::polygonCentroid(p4) * 1000000).toPoint().y() >= 325821);
+        QVERIFY((SceneGeometry::polygonCentroid(p4) * 1000000).toPoint().y() <= 325822);
 
         QPolygonF p5{{{273, 146}, {423, 496}, {415, 499}, {266, 149}}};
 //         QCOMPARE((SceneGeometry::polygonCentroid(p5) * 1000).toPoint(), QPoint(345669, 325821));
