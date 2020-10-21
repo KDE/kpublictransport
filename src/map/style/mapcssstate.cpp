afbd9456671908d9e5e9b9498c8681cbb90718e3
Volker Krause <vkrause@kde.org>
2020-10-21 17:47:51 +0200

    KOSMIndoorMap moved to its own repository

--- src/map/style/mapcssstate.cpp
+++
@@ -1,9 +0,0 @@
-/*
-    SPDX-FileCopyrightText: 2020 Volker Krause <vkrause@kde.org>
-
-    SPDX-License-Identifier: LGPL-2.0-or-later
-*/
-
-#include "mapcssstate_p.h"
-
-using namespace KOSMIndoorMap;
