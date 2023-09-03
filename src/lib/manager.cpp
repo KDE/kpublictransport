7008db678889b4c1eb1b477d4feecd4cfa658940
Volker Krause <vkrause@kde.org>
2023-09-03 11:17:38 +0200

    Move ÖBB coverage geometry to external files
    We need that for all other backend services too eventually, but this is
    enough for now to finish and test the integration.

--- src/lib/manager.cpp
+++ src/lib/manager.cpp
@@ -60,6 +60,7 @@ using namespace KPublicTransport;
 static inline void initResources() {
     Q_INIT_RESOURCE(asset_attributions);
     Q_INIT_RESOURCE(gbfs);
+    Q_INIT_RESOURCE(geometry);
     Q_INIT_RESOURCE(networks);
     Q_INIT_RESOURCE(network_certs);
     Q_INIT_RESOURCE(otp);
