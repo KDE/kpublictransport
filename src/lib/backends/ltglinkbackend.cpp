85185eccbf0019f75e47567a5f176181245ed63c
Jonah Brüchert <jbb@kaidan.im>
2024-01-05 05:15:13 +0100

    ltglink: Add support for fetching intermediate stops

--- src/lib/backends/ltglinkbackend.cpp
+++ src/lib/backends/ltglinkbackend.cpp
@@ -15,6 +15,7 @@
 #include <QJsonArray>
 #include <QTimeZone>
 
+#include "datatypes/stopover.h"
 #include "journeyrequest.h"
 #include "journeyreply.h"
 #include "locationrequest.h"
@@ -59,68 +60,119 @@ bool LTGLinkBackend::queryJourney(const JourneyRequest &req, JourneyReply *reply
 
     auto netReply = nam->get(QNetworkRequest(url));
     connect(netReply, &QNetworkReply::finished, this, [=, this]() {
-        const auto bytes = netReply->readAll();
-
-        auto jsonValue = QJsonDocument::fromJson(bytes);
+        auto jsonValue = QJsonDocument::fromJson(netReply->readAll());
 
         const auto journeysJson = jsonValue[u"Journeys"].toArray();
-        std::vector<Journey> journeys;
+
+        auto journeys = std::make_shared<std::vector<Journey>>();
+        auto runningRequests = std::make_shared<int>(0);
+
         for (const auto &j : journeysJson) {
+            auto journeyId = j[u"Id"].toString();
             auto sectionsJson = j[u"Legs"].toArray();
 
-            std::vector<JourneySection> sections;
-            for (const auto sectionJson : sectionsJson) {
-                auto originId = sectionJson[u"Origin"][u"Stop"][u"Id"].toInt();
-                auto originTimeZone = QTimeZone(sectionJson[u"Origin"][u"TimeZone"].toString().toUtf8());
+            QUrl routeUrl(QStringLiteral("https://bilietas.ltglink.lt/api/v2021/en-gb/journeys/%1/route?includeIntermediateStops=true").arg(journeyId));
+            auto routeReply = nam->get(QNetworkRequest(routeUrl));
 
-                auto actualDepartureTime = QDateTime::fromString(sectionJson[u"Origin"][u"ActualDepartureDateTime"].toString(), Qt::ISODate);
-                actualDepartureTime.setTimeZone(originTimeZone);
-                auto scheduledDepartureTime = QDateTime::fromString(sectionJson[u"Origin"][u"PlannedDepartureDateTime"].toString(), Qt::ISODate);
-                scheduledDepartureTime.setTimeZone(originTimeZone);
+            (*runningRequests)++;
 
-                auto destinationId = sectionJson[u"Destination"][u"Stop"][u"Id"].toInt();
-                auto destinationTimeZone = QTimeZone(sectionJson[u"Destination"][u"TimeZone"].toString().toUtf8());
+            connect(routeReply, &QNetworkReply::finished, this, [=, this]() {
+                auto route = QJsonDocument::fromJson(routeReply->readAll());
+                auto sectionStopsJson = route[u"Legs"].toArray();
 
-                auto actualArrivalTime = QDateTime::fromString(sectionJson[u"Destination"][u"ActualArrivalDateTime"].toString(), Qt::ISODate);
-                actualArrivalTime.setTimeZone(destinationTimeZone);
-                auto scheduledArrivalTime = QDateTime::fromString(sectionJson[u"Destination"][u"PlannedArrivalDateTime"].toString(), Qt::ISODate);
-                scheduledArrivalTime.setTimeZone(destinationTimeZone);
+                std::vector<JourneySection> sections;
+                for (const auto sectionJson : sectionsJson) {
+                    auto originId = sectionJson[u"Origin"][u"Stop"][u"Id"].toInt();
+                    auto originTimeZone = QTimeZone(sectionJson[u"Origin"][u"TimeZone"].toString().toUtf8());
 
-                // Filter out results that don't match the selected time frame
-                if (!LocalBackendUtils::isInSelectedTimeframe(actualDepartureTime, actualArrivalTime, req)) {
-                    continue;
+                    auto actualDepartureTime = QDateTime::fromString(sectionJson[u"Origin"][u"ActualDepartureDateTime"].toString(), Qt::ISODate);
+                    actualDepartureTime.setTimeZone(originTimeZone);
+                    auto scheduledDepartureTime = QDateTime::fromString(sectionJson[u"Origin"][u"PlannedDepartureDateTime"].toString(), Qt::ISODate);
+                    scheduledDepartureTime.setTimeZone(originTimeZone);
+
+                    auto destinationId = sectionJson[u"Destination"][u"Stop"][u"Id"].toInt();
+                    auto destinationTimeZone = QTimeZone(sectionJson[u"Destination"][u"TimeZone"].toString().toUtf8());
+
+                    auto actualArrivalTime = QDateTime::fromString(sectionJson[u"Destination"][u"ActualArrivalDateTime"].toString(), Qt::ISODate);
+                    actualArrivalTime.setTimeZone(destinationTimeZone);
+                    auto scheduledArrivalTime = QDateTime::fromString(sectionJson[u"Destination"][u"PlannedArrivalDateTime"].toString(), Qt::ISODate);
+                    scheduledArrivalTime.setTimeZone(destinationTimeZone);
+
+                    // Filter out results that don't match the selected time frame
+                    if (!LocalBackendUtils::isInSelectedTimeframe(actualDepartureTime, actualArrivalTime, req)) {
+                        continue;
+                    }
+
+                    auto lineNumber = sectionJson[u"Line"][u"Number"].toString();
+                    auto transportationType = sectionJson[u"Line"][u"TransportationType"][u"Id"].toString();
+
+                    Line line;
+                    line.setName(lineNumber);
+                    line.setMode(transportationType == u"LINE_TRANSPORTATION_TYPE.TRAIN" ? Line::Mode::Train : Line::Mode::Unknown);
+
+                    Route route;
+                    route.setLine(line);
+
+                    JourneySection section;
+                    section.setFrom(lookupStation(originId));
+                    section.setTo(lookupStation(destinationId));
+                    section.setScheduledDepartureTime(scheduledDepartureTime);
+                    section.setExpectedDepartureTime(actualDepartureTime);
+                    section.setScheduledArrivalTime(scheduledArrivalTime);
+                    section.setExpectedArrivalTime(actualArrivalTime);
+                    section.setMode(JourneySection::PublicTransport);
+                    section.setRoute(route);
+
+                    sections.push_back(std::move(section));
                 }
 
-                auto lineNumber = sectionJson[u"Line"][u"Number"].toString();
-                auto transportationType = sectionJson[u"Line"][u"TransportationType"][u"Id"].toString();
+                for (int i = 0; i < sectionStopsJson.size() && i < int(sections.size()); i++) {
+                    auto sectionStopJson = sectionStopsJson[i];
 
-                Line line;
-                line.setName(lineNumber);
-                line.setMode(transportationType == u"LINE_TRANSPORTATION_TYPE.TRAIN" ? Line::Mode::Train : Line::Mode::Unknown);
+                    std::vector<Stopover> stopovers;
+                    for (const auto &stopJson : sectionStopJson[u"IntermediateStops"].toArray()) {
+                        QTimeZone arrivalTimeZone(stopJson[u"ArrivalDateTimeZone"].toString().toUtf8());
+                        QTimeZone departureTimeZone(stopJson[u"DepartureDateTimeZone"].toString().toUtf8());
 
-                Route route;
-                route.setLine(line);
+                        // Unfortunately no coordinates included in reply :(
+                        Location location;
+                        location.setName(stopJson[u"StopName"].toString());
+                        location.setCountry(QLocale::territoryToString(arrivalTimeZone.territory()));
+                        location.setTimeZone(arrivalTimeZone);
 
-                JourneySection section;
-                section.setFrom(lookupStation(originId));
-                section.setTo(lookupStation(destinationId));
-                section.setScheduledDepartureTime(scheduledDepartureTime);
-                section.setExpectedDepartureTime(actualDepartureTime);
-                section.setScheduledArrivalTime(scheduledArrivalTime);
-                section.setExpectedArrivalTime(actualArrivalTime);
-                section.setMode(JourneySection::PublicTransport);
-                section.setRoute(route);
+                        auto arrivalDateTime = QDateTime::fromString(stopJson[u"ArrivalDateTime"].toString(), Qt::ISODate);
+                        arrivalDateTime.setTimeZone(arrivalTimeZone);
 
-                sections.push_back(std::move(section));
-            }
+                        auto departureDateTime = QDateTime::fromString(stopJson[u"DepartureDateTime"].toString(), Qt::ISODate);
+                        arrivalDateTime.setTimeZone(departureTimeZone);
 
-            Journey journey;
-            journey.setSections(std::move(sections));
+                        Stopover stopover;
+                        stopover.setStopPoint(location);
+                        stopover.setScheduledArrivalTime(arrivalDateTime);
+                        stopover.setScheduledDepartureTime(departureDateTime);
 
-            journeys.push_back(std::move(journey));
-        }
+                        stopovers.push_back(std::move(stopover));
+                    }
 
-        addResult(reply, this, std::move(journeys));
+                    sections[i].setIntermediateStops(std::move(stopovers));
+                }
+
+                Journey journey;
+                journey.setSections(std::move(sections));
+
+                journeys->push_back(std::move(journey));
+                (*runningRequests)--;
+
+                if (*runningRequests == 0) {
+                    Attribution attribution;
+                    attribution.setName(QStringLiteral("LTG Link"));
+                    attribution.setUrl(QUrl(QStringLiteral("https://ltglink.lt")));
+                    addAttributions(reply, {attribution});
+
+                    addResult(reply, this, std::move(*journeys));
+                }
+            });
+        };
 
         netReply->deleteLater();
     });
@@ -161,7 +213,14 @@ void LTGLinkBackend::downloadStationData(Reply *reply, QNetworkAccessManager *na
 
     QUrl url(QStringLiteral("https://cms.ltglink.turnit.com/api/turnit/search"));
     QUrlQuery urlQuery;
-    urlQuery.addQueryItem(QStringLiteral("locale"), QLocale::languageToCode(QLocale().language()));
+
+    // The API falls back to lithuanian, so we want to force english most of the time
+    if (QLocale().language() == QLocale::Language::Lithuanian) {
+        urlQuery.addQueryItem(QStringLiteral("locale"), QStringLiteral("lt"));
+    } else {
+        urlQuery.addQueryItem(QStringLiteral("locale"), QStringLiteral("en"));
+    }
+
     url.setQuery(urlQuery);
 
     auto *netReply = nam->get(QNetworkRequest(url));
