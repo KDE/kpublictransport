bf94e90daa65b92c9c23743ad9f8dc4c64af2942
Volker Krause <vkrause@kde.org>
2024-02-12 20:06:46 +0100

    Use [[nodiscard]] consistently and extend API docs a bit

--- src/lib/backends/abstractbackend.h
+++ src/lib/backends/abstractbackend.h
@@ -40,6 +40,16 @@ class VehicleLayoutRequest;
 
 /** Abstract base class for transport provider backends.
  *  @internal exported for tooling only
+ *
+ *  ## Network operations
+ *  - For the built-in SSL workarounds to work, call applySslConfiguration() on network requests.
+ *  - Parent QNetworkReply objects to the corresponding KPublicTransport::Reply object. This ensures
+ *    cancelation of operations are propagated correctly.
+ *  - For the backend communication logging to work, call logRequest() on all network requests and the corresponding POST data (if any),
+ *    and call logReply() on all network replies.
+ *  - Complete operations by either calling addResult() or addError(). This must happen for all
+ *    query methods that returned @c true under all conditions, otherwise the corresponding operation
+ *    is stuck forever.
  */
 class KPUBLICTRANSPORT_EXPORT AbstractBackend
 {
@@ -54,13 +64,13 @@ public:
     /** Identifier for this backend.
      *  Use e.g. for distinguishing backend-specific cache locations etc.
      */
-    QString backendId() const;
+    [[nodiscard]] QString backendId() const;
     void setBackendId(const QString &id);
 
     /** Static timezone for all times used by and provided by the backend.
      *  Set to invalid if the backend supports multiple timezones.
      */
-    QTimeZone timeZone() const;
+    [[nodiscard]] QTimeZone timeZone() const;
     void setTimeZone(const QTimeZone &tz);
 
     /** Supported languages.
@@ -69,7 +79,7 @@ public:
      *  The first language in QLocale::uiLanguages found in this list should be requested.
      *  @see preferredLanguage()
      */
-    QStringList supportedLanguages() const;
+    [[nodiscard]] QStringList supportedLanguages() const;
     void setSupportedLanguages(const QStringList &langs);
 
     /** Returns the language that should be requested.
@@ -77,11 +87,12 @@ public:
      *  in the list of supported languages, if that's not the case the first supported
      *  language of the backend, or if that's not set, English by default.
      */
-    QString preferredLanguage() const;
+    [[nodiscard]] QString preferredLanguage() const;
 
     /** Called after all settings have been set on this backend. */
     virtual void init();
 
+    /** Backend capabilities. */
     enum Capability {
         NoCapability = 0,
         Secure = 1, ///< uses transport encryption
@@ -94,12 +105,12 @@ public:
     Q_DECLARE_FLAGS(Capabilities, Capability)
 
     /** Returns the capabilities of this backend. */
-    virtual Capabilities capabilities() const;
+    [[nodiscard]] virtual Capabilities capabilities() const;
     /** Checks if this backend has @p capability. */
-    inline bool hasCapability(Capability capability) const { return capabilities() & capability; };
+    [[nodiscard]] inline bool hasCapability(Capability capability) const { return capabilities() & capability; };
 
     /** Returns the static attribution information for this backend. */
-    Attribution attribution() const;
+    [[nodiscard]] Attribution attribution() const;
     /** Sets the static attribution information for this backend. */
     void setAttribution(const Attribution &attr);
 
@@ -112,28 +123,37 @@ public:
     };
     /** Returns whether or not a location query has to be performed for the given
      *  Location before doing departure or journey queries for it.
+     *
+     *  Reimplement this if stopover or journey queries for this backend have
+     *  specific requirements on the location they refer so, such as coordinates
+     *  or a certain identifier type being present.
      */
-    virtual bool needsLocationQuery(const Location &loc, QueryType type) const;
+    [[nodiscard]] virtual bool needsLocationQuery(const Location &loc, QueryType type) const;
 
     /** Perform a journey query.
      *  @return @c true if performing an async operation, @c false otherwise.
      */
-    virtual bool queryJourney(const JourneyRequest &request, JourneyReply *reply, QNetworkAccessManager *nam) const;
+    [[nodiscard]] virtual bool queryJourney(const JourneyRequest &request, JourneyReply *reply, QNetworkAccessManager *nam) const;
 
     /** Perform a departure query.
      *  @return @c true if performing an async operation, @c false otherwise.
      */
-    virtual bool queryStopover(const StopoverRequest &request, StopoverReply *reply, QNetworkAccessManager *nam) const;
+    [[nodiscard]] virtual bool queryStopover(const StopoverRequest &request, StopoverReply *reply, QNetworkAccessManager *nam) const;
 
     /** Perform a location query.
+     *
+     *  Typically you'll have to handle two cases in here:
+     *  - request.hasCoordinate() being @c true
+     *  - request.name() being set
+     *
      *  @return @c true if performing an async operation, @c false otherwise.
      */
-    virtual bool queryLocation(const LocationRequest &request, LocationReply *reply, QNetworkAccessManager *nam) const;
+    [[nodiscard]] virtual bool queryLocation(const LocationRequest &request, LocationReply *reply, QNetworkAccessManager *nam) const;
 
     /** Perform a vehicle layout query.
      *  @return @c true if performing an async operation, @c false otherwise.
      */
-    virtual bool queryVehicleLayout(const VehicleLayoutRequest &request, VehicleLayoutReply *reply, QNetworkAccessManager *nam) const;
+    [[nodiscard]] virtual bool queryVehicleLayout(const VehicleLayoutRequest &request, VehicleLayoutReply *reply, QNetworkAccessManager *nam) const;
 
 protected:
     /** Helper function to call non-public Reply API. */
@@ -150,12 +170,14 @@ protected:
     static void addAttributions(Reply *reply, std::vector<Attribution> &&attributions);
 
     /** Extract the request context for the current backend from @p request. */
-    template <typename ReqT> inline RequestContext requestContext(const ReqT &request) const
+    template <typename ReqT>
+    [[nodiscard]] inline RequestContext requestContext(const ReqT &request) const
     {
         return request.context(this);
     }
     /** Extract the backend-specific data from the request context for the current backend from @p request. */
-    template <typename ReqT> inline QVariant requestContextData(const ReqT &request) const
+    template <typename ReqT>
+    [[nodiscard]] inline QVariant requestContextData(const ReqT &request) const
     {
         return request.context(this).backendData;
     }
@@ -173,7 +195,7 @@ protected:
      *  Done automatically in logRequest/logReply, but can be useful for backends if producing
      *  the necessary input is expensive.
      */
-    bool isLoggingEnabled() const;
+    [[nodiscard]] bool isLoggingEnabled() const;
 
     template <typename ReqT>
     void logRequest(const ReqT &request, const QNetworkRequest &netRequest, const QByteArray &postData = {}) const
@@ -200,7 +222,7 @@ protected:
 
 private:
     Q_DISABLE_COPY(AbstractBackend)
-    QString logDir() const;
+    [[nodiscard]] QString logDir() const;
     void logRequest(const char *typeName, const QJsonObject &requestData, const QNetworkRequest &netRequest, const QByteArray &postData) const;
     void logReply(const char *typeName, QNetworkReply *netReply, const QByteArray &data) const;
     void setCustomCaCertificate(const QString &caCert);
@@ -243,7 +265,7 @@ public:
         Q_EMIT finished();
     }
 
-    const std::optional<T> &result() const {
+    [[nodiscard]] const std::optional<T> &result() const {
         return m_result;
     }
 };
