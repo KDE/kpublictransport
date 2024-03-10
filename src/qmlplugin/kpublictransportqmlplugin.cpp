6f54b819f6992de2637537a46ee82ea6f87c80c3
Nicolas Fella <nicolas.fella@gmx.de>
2024-03-10 17:05:39 +0100

    Port QML import to declarative type registration

--- src/qmlplugin/kpublictransportqmlplugin.cpp
+++ src/qmlplugin/kpublictransportqmlplugin.cpp
@@ -10,26 +10,18 @@
 #include <QQmlEngine>
 #include <QQmlContext>
 
-#include <KPublicTransport/BackendModel>
 #include <KPublicTransport/CoverageArea>
 #include <KPublicTransport/Equipment>
 #include <KPublicTransport/IndividualTransport>
 #include <KPublicTransport/Journey>
-#include <KPublicTransport/JourneyQueryModel>
 #include <KPublicTransport/JourneyRequest>
 #include <KPublicTransport/Line>
-#include <KPublicTransport/LocationHistoryModel>
-#include <KPublicTransport/LocationQueryModel>
 #include <KPublicTransport/LocationRequest>
-#include <KPublicTransport/Manager>
 #include <KPublicTransport/Path>
-#include <KPublicTransport/PathModel>
 #include <KPublicTransport/Platform>
 #include <KPublicTransport/RentalVehicle>
 #include <KPublicTransport/StopoverRequest>
-#include <KPublicTransport/StopoverQueryModel>
 #include <KPublicTransport/Vehicle>
-#include <KPublicTransport/VehicleLayoutQueryModel>
 #include <KPublicTransport/VehicleLayoutRequest>
 
 void KPublicTransportQmlPlugin::registerTypes(const char*)
@@ -52,13 +44,11 @@ void KPublicTransportQmlPlugin::registerTypes(const char*)
     qmlRegisterUncreatableMetaObject(KPublicTransport::Type::staticMetaObject, "org.kde.kpublictransport", 1, 0, #Type, {});
 
     REGISTER_METAOBJECT(CoverageArea)
-    REGISTER_METAOBJECT(Disruption)
     REGISTER_METAOBJECT(Equipment)
     REGISTER_METAOBJECT(IndividualTransport)
     REGISTER_METAOBJECT(JourneyRequest)
     REGISTER_METAOBJECT(JourneySection)
     REGISTER_METAOBJECT(Line)
-    REGISTER_METAOBJECT(Load)
     REGISTER_METAOBJECT(Location)
     REGISTER_METAOBJECT(LocationRequest)
     REGISTER_METAOBJECT(PathSection)
@@ -68,15 +58,6 @@ void KPublicTransportQmlPlugin::registerTypes(const char*)
     REGISTER_METAOBJECT(Vehicle)
     REGISTER_METAOBJECT(VehicleLayoutRequest)
     REGISTER_METAOBJECT(VehicleSection)
-
-    qmlRegisterType<KPublicTransport::Manager>("org.kde.kpublictransport", 1, 0, "Manager");
-    qmlRegisterType<KPublicTransport::JourneyQueryModel>("org.kde.kpublictransport", 1, 0, "JourneyQueryModel");
-    qmlRegisterType<KPublicTransport::LocationHistoryModel>("org.kde.kpublictransport", 1, 0, "LocationHistoryModel");
-    qmlRegisterType<KPublicTransport::LocationQueryModel>("org.kde.kpublictransport", 1, 0, "LocationQueryModel");
-    qmlRegisterType<KPublicTransport::BackendModel>("org.kde.kpublictransport", 1, 0, "BackendModel");
-    qmlRegisterType<KPublicTransport::StopoverQueryModel>("org.kde.kpublictransport", 1, 0, "StopoverQueryModel");
-    qmlRegisterType<KPublicTransport::VehicleLayoutQueryModel>("org.kde.kpublictransport", 1, 0, "VehicleLayoutQueryModel");
-    qmlRegisterType<KPublicTransport::PathModel>("org.kde.kpublictransport", 1, 0, "PathModel");
 
     qmlRegisterSingletonType("org.kde.kpublictransport", 1, 0, "LineMetaData", [](QQmlEngine*, QJSEngine *engine) -> QJSValue {
         return engine->toScriptValue(KPublicTransport::LineMetaDataWrapper());
--- src/qmlplugin/kpublictransportqmlplugin.h
+++ src/qmlplugin/kpublictransportqmlplugin.h
@@ -7,7 +7,87 @@
 #ifndef KPUBLICTRANSPORTQMLPLUGIN_H
 #define KPUBLICTRANSPORTQMLPLUGIN_H
 
+#include <QQmlEngine>
 #include <QQmlExtensionPlugin>
+
+#include <KPublicTransport/BackendModel>
+#include <KPublicTransport/JourneyQueryModel>
+#include <KPublicTransport/LocationHistoryModel>
+#include <KPublicTransport/LocationQueryModel>
+#include <KPublicTransport/Manager>
+#include <KPublicTransport/PathModel>
+#include <KPublicTransport/StopoverQueryModel>
+#include <KPublicTransport/VehicleLayoutQueryModel>
+
+struct ManagerForeign {
+    Q_GADGET
+    QML_ELEMENT
+    QML_FOREIGN(KPublicTransport::Manager)
+    QML_NAMED_ELEMENT(Manager)
+};
+
+struct StopoverQueryModelForeign {
+    Q_GADGET
+    QML_ELEMENT
+    QML_FOREIGN(KPublicTransport::StopoverQueryModel)
+    QML_NAMED_ELEMENT(StopoverQueryModel)
+};
+
+struct JourneyQueryModelForeign {
+    Q_GADGET
+    QML_ELEMENT
+    QML_FOREIGN(KPublicTransport::JourneyQueryModel)
+    QML_NAMED_ELEMENT(JourneyQueryModel)
+};
+
+struct LocationHistoryModelForeign {
+    Q_GADGET
+    QML_ELEMENT
+    QML_FOREIGN(KPublicTransport::LocationHistoryModel)
+    QML_NAMED_ELEMENT(LocationHistoryModel)
+};
+
+struct LocationQueryModelForeign {
+    Q_GADGET
+    QML_ELEMENT
+    QML_FOREIGN(KPublicTransport::LocationQueryModel)
+    QML_NAMED_ELEMENT(LocationQueryModel)
+};
+
+struct BackendModelForeign {
+    Q_GADGET
+    QML_ELEMENT
+    QML_FOREIGN(KPublicTransport::BackendModel)
+    QML_NAMED_ELEMENT(BackendModel)
+};
+
+struct VehicleLayoutQueryModelForeign {
+    Q_GADGET
+    QML_ELEMENT
+    QML_FOREIGN(KPublicTransport::VehicleLayoutQueryModel)
+    QML_NAMED_ELEMENT(VehicleLayoutQueryModel)
+};
+
+struct PathModelForeign {
+    Q_GADGET
+    QML_ELEMENT
+    QML_FOREIGN(KPublicTransport::PathModel)
+    QML_NAMED_ELEMENT(PathModel)
+};
+
+namespace DisruptionForeign
+{
+Q_NAMESPACE
+QML_NAMED_ELEMENT(Disruption)
+QML_FOREIGN_NAMESPACE(KPublicTransport::Disruption)
+};
+
+namespace LoadForeign
+{
+Q_NAMESPACE
+QML_NAMED_ELEMENT(Load)
+QML_FOREIGN_NAMESPACE(KPublicTransport::Load)
+};
 
 class KPublicTransportQmlPlugin : public QQmlExtensionPlugin
 {
