18987cecf4d6f7365f1708d5958b977358b3d4ba
Alistair Francis <alistair@alistair23.me>
2024-03-20 19:50:58 +1000

    lib: networks: Rename Transitous to be universal
    Rename the Transitous network to be universal and add a test for
    Australia.
    
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- tests/querytest.cpp
+++ tests/querytest.cpp
@@ -65,6 +65,7 @@ private Q_SLOTS:
         // TODO un_gbfs - this needs special-casing given it cannot do journey and departure searches
         QTest::newRow("un_navitia") << "un_navitia" << loc("Paris Gare de Lyon", 48.84444, 2.37412) << loc("Paris Aéroport Charles de Gaulle 2 TGV", 49.00423, 2.57215);
         QTest::newRow("au_navitia") << "au_navitia" << loc("King George Square Station",  -27.468903869, 153.0242799) << loc("Brisbane International Airport", -27.404467, 153.108597);
+        QTest::newRow("un_transitous") << "un_transitous" << loc("King George Square Bus Station",  -27.468903869, 153.0242799) << loc("Brisbane International Airport", -27.404467, 153.108597);
 
         // national
         QTest::newRow("at_oebb") << "at_oebb" << loc("Wien Hauptbahnhof", 48.18282, 16.37859) << loc("Linz/Donau Hbf", 48.29058, 14.29018);
