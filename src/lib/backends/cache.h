0b4b0424875bbe7e05aa9a858886d9866e4b601a
Volker Krause <vkrause@kde.org>
2024-10-21 17:20:50 +0200

    Remove pre-C++20 compat code

--- src/lib/backends/cache.h
+++ src/lib/backends/cache.h
@@ -42,37 +42,32 @@ template <typename T> struct CacheEntry
     CacheHitType type = CacheHitType::Miss;
 };
 
-/** C++20 forward compatibility */
-namespace chrono {
-    typedef std::chrono::duration<std::chrono::seconds::rep, std::ratio<86400>> days;
-}
-
 /** Query result caching functions.
  *  @internal exported for unit tests only
  */
 namespace Cache
 {
     /** Add data and corresponding attribution information to the cache. */
-    KPUBLICTRANSPORT_EXPORT void addLocationCacheEntry(const QString &backendId, const QString &cacheKey, const std::vector<Location> &data, const std::vector<Attribution> &attributions, std::chrono::seconds ttl = chrono::days(30));
+    KPUBLICTRANSPORT_EXPORT void addLocationCacheEntry(const QString &backendId, const QString &cacheKey, const std::vector<Location> &data, const std::vector<Attribution> &attributions, std::chrono::seconds ttl = std::chrono::days(30));
     /** Add negative cache entry for location queries, ie. remember a result could not be found. */
-    KPUBLICTRANSPORT_EXPORT void addNegativeLocationCacheEntry(const QString &backendId, const QString &cacheKey, std::chrono::seconds ttl = chrono::days(30));
+    KPUBLICTRANSPORT_EXPORT void addNegativeLocationCacheEntry(const QString &backendId, const QString &cacheKey, std::chrono::seconds ttl = std::chrono::days(30));
     /** Perform cache lookup for location results. */
     KPUBLICTRANSPORT_EXPORT CacheEntry<Location> lookupLocation(const QString &backendId, const QString &cacheKey);
 
     /** Add negative cache entry for departure queries, ie. remember a result could not be found. */
-    void addNegativeStopoverCacheEntry(const QString &backendId, const QString &cacheKey, std::chrono::seconds ttl = chrono::days(30));
+    void addNegativeStopoverCacheEntry(const QString &backendId, const QString &cacheKey, std::chrono::seconds ttl = std::chrono::days(30));
     /** Perform cache lookup for departure results. */
     CacheEntry<Stopover> lookupStopover(const QString &backendId, const QString &cacheKey);
 
     /** Add negative cache entry for departure queries, ie. remember a result could not be found. */
-    void addNegativeJourneyCacheEntry(const QString &backendId, const QString &cacheKey, std::chrono::seconds ttl = chrono::days(30));
+    void addNegativeJourneyCacheEntry(const QString &backendId, const QString &cacheKey, std::chrono::seconds ttl = std::chrono::days(30));
     /** Perform cache lookup for departure results. */
     CacheEntry<Journey> lookupJourney(const QString &backendId, const QString &cacheKey);
 
     /** Add a positive cache entry for a vehicle layout query. */
     void addVehicleLayoutCacheEntry(const QString &backendId, const QString &cacheKey, const Stopover &data, const std::vector<Attribution> &attributions, std::chrono::seconds ttl = std::chrono::minutes(1));
     /** Add negative cache entry for vehicle layout queries. */
-    void addNegativeVehicleLayoutCacheEntry(const QString &backendId, const QString &cacheKey, std::chrono::seconds ttl = chrono::days(30));
+    void addNegativeVehicleLayoutCacheEntry(const QString &backendId, const QString &cacheKey, std::chrono::seconds ttl = std::chrono::days(30));
     /** Perform cache lookup for vehicle layout results. */
     CacheEntry<Stopover> lookupVehicleLayout(const QString &backendId, const QString &cacheKey);
 
