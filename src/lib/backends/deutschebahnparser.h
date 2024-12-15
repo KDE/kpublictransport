6c1801afd5e6af8897d4fa3f036a678549119fbd
Volker Krause <vkrause@kde.org>
2024-12-15 17:11:48 +0100

    Add support for DB's proxied Hafas API
    Mostly motivated by rumors of the imminent shutdown of the "real" Hafas
    endpoint. There's a few information we are not getting this way anymore
    (e.g. stopover coordinates, shapes), so for now this is mainly meant as
    an emergency backup option.
    
    We keep using the old Hafas API for now, in a local build this is available
    as a second backend for testing.

---
+++ src/lib/backends/deutschebahnparser.h
@@ -0,0 +1,32 @@
+/*
+    SPDX-FileCopyrightText: 2024 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#ifndef KPUBLICTRANSPORT_DEUTSCHEBAHNPARSER_H
+#define KPUBLICTRANSPORT_DEUTSCHEBAHNPARSER_H
+
+#include <KPublicTransport/Journey>
+#include <KPublicTransport/Location>
+#include <KPublicTransport/Stopover>
+
+class QJsonArray;
+
+namespace KPublicTransport {
+
+class HafasMgateParser;
+
+/** Deutsche Bahn Hafas proxy response parser.
+ *  @internal exported for unit tests only
+ */
+class DeutscheBahnParser
+{
+public:
+    [[nodiscard]] static std::vector<Location> parseLocations(const QByteArray &data, const HafasMgateParser &parser);
+    [[nodiscard]] static std::vector<Stopover> parseStopovers(const QJsonArray &stopsArray, bool isDeparture, const HafasMgateParser &parser);
+    [[nodiscard]] static std::vector<Journey> parseJourneys(const QJsonArray &journeysArray, const HafasMgateParser &parser);
+};
+
+}
+
+#endif
