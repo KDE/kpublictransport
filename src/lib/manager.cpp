693d0c3745c09104169dfb2fa3e107e3d1f08368
Volker Krause <vkrause@kde.org>
2025-04-28 17:31:52 +0200

    Treat neither a positive nor a negative cache hit as missing backend
    In both cases we do have a viable backend and that has previously
    provided a result.

--- src/lib/manager.cpp
+++ src/lib/manager.cpp
@@ -955,7 +955,7 @@ VehicleLayoutReply* Manager::queryVehicleLayout(const VehicleLayoutRequest &req)
 {
     auto reply = d->makeReply<VehicleLayoutReply>(req);
     int pendingOps = 0;
-    int negativeCacheHit = 0;
+    int cacheHit = 0;
 
     // validate input
     if (!req.isValid()) {
@@ -980,10 +980,11 @@ VehicleLayoutReply* Manager::queryVehicleLayout(const VehicleLayoutRequest &req)
             auto cache = Cache::lookupVehicleLayout(backend.identifier(), req.cacheKey());
             switch (cache.type) {
                 case CacheHitType::Negative:
-                    ++negativeCacheHit;
+                    ++cacheHit;
                     qCDebug(Log) << "Negative cache hit for backend" << backend.identifier();
                     break;
                 case CacheHitType::Positive:
+                    ++cacheHit;
                     qCDebug(Log) << "Positive cache hit for backend" << backend.identifier();
                     if (cache.data.size() == 1) {
                         reply->addAttributions(std::move(cache.attributions));
@@ -1004,7 +1005,7 @@ VehicleLayoutReply* Manager::queryVehicleLayout(const VehicleLayoutRequest &req)
         }
     }
 
-    if (pendingOps == 0 && negativeCacheHit == 0) {
+    if (pendingOps == 0 && cacheHit == 0) {
         reply->addError(Reply::NoBackend, u"No viable backend found."_s);
     }
     reply->setPendingOps(pendingOps);
