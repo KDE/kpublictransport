1d42f6cfd7a96adf01a2c9d01bb30cf607bb85ab
Volker Krause <vkrause@kde.org>
2025-06-07 12:11:35 +0200

    Update MockNetworkAccessManager to the version from Itinerary
    That can actually deliver replies.

--- autotests/mocknetworkaccessmanager.h
+++ autotests/mocknetworkaccessmanager.h
@@ -2,9 +2,12 @@
     SPDX-FileCopyrightText: 2023 Volker Krause <vkrause@kde.org>
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
+#pragma once
 
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
 
+#include <queue>
 #include <vector>
 
 class MockNetworkAccessManager : public QNetworkAccessManager
@@ -19,6 +22,15 @@ public:
 
     std::vector<Request> requests;
 
+    struct Reply {
+        QNetworkReply::NetworkError error = QNetworkReply::NetworkSessionFailedError;
+        int statusCode = 0;
+        QByteArray data = {};
+        QString errorMessage = QStringLiteral("no pending reply available");
+    };
+
+    std::queue<Reply> replies;
+
 protected:
-    QNetworkReply* createRequest(QNetworkAccessManager::Operation op, const QNetworkRequest &originalReq, QIODevice *outgoingData = nullptr) override;
+    QNetworkReply *createRequest(QNetworkAccessManager::Operation op, const QNetworkRequest &originalReq, QIODevice *outgoingData = nullptr) override;
 };
