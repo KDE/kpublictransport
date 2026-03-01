329d55f5f20795d44c9a98897fdaaf5033f6b5e3
Volker Krause <vkrause@kde.org>
2026-03-01 13:45:00 +0100

    Add infrastructure for dealing with SIRI mode types
    This will replace the partial solution we have for this in
    the OTP and OJP backends, and allow proper reverse mapping
    based on the specific capabilities of the backend.
    
    It's build in a way that everything is derived from the enum
    declarations and the mapping tables to our mode types,
    without having to maunally maintain string tables or
    other auxiliary mappings.
    
    (cherry picked from commit bf931de7704ac9c71d73eedc8a74246e79e4c8de)

---
+++ src/lib/siri/mode.cpp
@@ -0,0 +1,287 @@
+/*
+    SPDX-FileCopyrightText: 2026 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "mode.h"
+
+#include <datatypes/json_p.h>
+
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QMetaEnum>
+
+using namespace Qt::Literals;
+using namespace KPublicTransport;
+
+template <typename ModeT>
+struct ModeMap {
+    ModeT siriMode;
+    Line::Mode mode;
+};
+
+template <typename T>
+static constexpr const T mode_map = {};
+
+// complete mapping between SIRI modes and our modes
+template <>
+constexpr const ModeMap<Siri::Mode::VehicleMode> mode_map<Siri::Mode::VehicleMode>[] = {
+    { Siri::Mode::VehicleMode::air, Line::Air },
+    { Siri::Mode::VehicleMode::bus, Line::Bus },
+    { Siri::Mode::VehicleMode::cableway, Line::AerialLift },
+    { Siri::Mode::VehicleMode::water, Line::Ferry },
+    { Siri::Mode::VehicleMode::funicular, Line::Funicular },
+    { Siri::Mode::VehicleMode::lift, Line::AerialLift },
+    { Siri::Mode::VehicleMode::rail, Line::Train },
+    { Siri::Mode::VehicleMode::snowAndIce, Line::Unknown },
+    { Siri::Mode::VehicleMode::metro, Line::Metro },
+    { Siri::Mode::VehicleMode::taxi, Line::Taxi },
+    { Siri::Mode::VehicleMode::tram, Line::Tramway },
+    { Siri::Mode::VehicleMode::trolleybus, Line::Bus },
+    { Siri::Mode::VehicleMode::monorail, Line::Train },
+    { Siri::Mode::VehicleMode::coach, Line::Coach },
+    { Siri::Mode::VehicleMode::carpool, Line::RideShare },
+    { Siri::Mode::VehicleMode::other, Line::Unknown },
+    { Siri::Mode::VehicleMode::unknown, Line::Unknown },
+};
+
+// partial maps between SIRI submodes and our modes, only containing deviations from the default
+template<>
+constexpr const ModeMap<Siri::Mode::BusSubmode> mode_map<Siri::Mode::BusSubmode>[] = {
+    { Siri::Mode::BusSubmode::demandAndResponseBus, Line::Taxi },
+};
+
+template<>
+constexpr const ModeMap<Siri::Mode::MetroSubmode> mode_map<Siri::Mode::MetroSubmode>[] = {
+    { Siri::Mode::MetroSubmode::urbanRailway, Line::RapidTransit },
+};
+
+template <>
+constexpr const ModeMap<Siri::Mode::RailSubmode> mode_map<Siri::Mode::RailSubmode>[] = {
+    { Siri::Mode::RailSubmode::local, Line::LocalTrain },
+    { Siri::Mode::RailSubmode::highSpeedRail, Line::LongDistanceTrain },
+    { Siri::Mode::RailSubmode::suburbanRailway, Line::RapidTransit },
+    { Siri::Mode::RailSubmode::regionalRail, Line::LocalTrain },
+    { Siri::Mode::RailSubmode::longDistance, Line::LongDistanceTrain },
+    { Siri::Mode::RailSubmode::international, Line::LongDistanceTrain },
+    { Siri::Mode::RailSubmode::sleeperRailService, Line::LongDistanceTrain },
+    { Siri::Mode::RailSubmode::nightRail, Line::LongDistanceTrain },
+    { Siri::Mode::RailSubmode::airportLinkRail, Line::RapidTransit },
+    { Siri::Mode::RailSubmode::railShuttle, Line::Shuttle },
+};
+
+template <>
+constexpr const ModeMap<Siri::Mode::WaterSubmode> mode_map<Siri::Mode::WaterSubmode>[] = {
+    { Siri::Mode::WaterSubmode::postBoat, Line::Boat },
+    { Siri::Mode::WaterSubmode::airportBoatLink, Line::Boat },
+    { Siri::Mode::WaterSubmode::sightseeingService, Line::Boat },
+    { Siri::Mode::WaterSubmode::schoolBoat, Line::Boat },
+    { Siri::Mode::WaterSubmode::riverBus, Line::Boat },
+    { Siri::Mode::WaterSubmode::canalBarge, Line::Boat },
+};
+
+template <typename T>
+[[nodiscard]] static T modeFromString(QStringView mode)
+{
+    auto me = QMetaEnum::fromType<T>();
+    bool ok = false;
+    const auto m = static_cast<T>(me.keyToValue(mode.toUtf8().constData(), &ok));
+    return ok ? m : T::unknown;
+}
+
+static Line::Mode modeToMode(Siri::Mode::VehicleMode siriMode)
+{
+    const auto it = std::ranges::find_if(mode_map<Siri::Mode::VehicleMode>, [siriMode](const auto &m) { return m.siriMode == siriMode; });
+    return (it == std::end(mode_map<Siri::Mode::VehicleMode>)) ? Line::Unknown : (*it).mode;
+}
+
+template <typename T>
+[[nodiscard]] static std::optional<Line::Mode> submodeToMode(T mode)
+{
+    if constexpr (requires(T) { mode_map<T>[0]; }) {
+        const auto it = std::ranges::find_if(mode_map<T>, [mode](const auto &m) { return m.siriMode == mode; });
+        if (it == std::end(mode_map<T>)) {
+            return {};
+        }
+        return (*it).mode;
+    }
+    return {};
+}
+
+Siri::Mode Siri::Mode::fromString(QStringView mode, QStringView subMode)
+{
+    Mode m(modeFromString<Siri::Mode::VehicleMode>(mode));
+    m.m_submode = submodeFromString(detail::num<>(), subMode, m.mode());
+    if (m.mode() == VehicleMode::unknown && m.hasSubmode()) {
+        m.m_mode = std::visit([](auto &&m) -> VehicleMode { return modeForSubmode(m); }, *m.m_submode);
+    }
+
+    if (m.mode() == VehicleMode::unknown && (!mode.isEmpty() || !subMode.isEmpty()) && mode != QLatin1StringView(Mode::toString(VehicleMode::unknown))) {
+        qDebug() << "unable to resolve SIRI mode:" << mode << subMode;
+    }
+
+    return m;
+}
+
+Line::Mode Siri::Mode::toMode() const
+{
+    if (m_submode) {
+        if (auto m = std::visit([](auto &&m) -> std::optional<Line::Mode> { return submodeToMode(m); }, *m_submode); m) {
+            return *m;
+        }
+    }
+    return modeToMode(m_mode);
+}
+
+std::vector<Siri::Mode> Siri::Mode::fromMode(Line::Mode mode)
+{
+    std::vector<Siri::Mode> result;
+    fromModeSubmodeImpl<Siri::Mode::VehicleMode>(mode, result);
+    fromModeSubmode(detail::num<>(), mode, result);
+    return result;
+}
+
+std::vector<Siri::Mode> Siri::Mode::fromModes(const std::vector<Line::Mode> &modes)
+{
+    std::vector<Siri::Mode> result;
+    for (auto m : modes) {
+        fromModeSubmodeImpl<Siri::Mode::VehicleMode>(m, result);
+    }
+    // processing all submodes last is necessary for the below deduplication logic to work
+    for (auto m : modes) {
+        fromModeSubmode(detail::num<>(), m, result);
+    }
+    return result;
+}
+
+std::strong_ordering Siri::Mode::operator<=>(const Mode &other) const
+{
+    const auto modeCmp = m_mode <=> other.m_mode;
+    if (modeCmp == std::strong_ordering::equal) {
+        if (!m_submode && !other.m_submode) {
+            return std::strong_ordering::equal;
+        }
+        if (!m_submode) {
+            return std::strong_ordering::less;
+        }
+        if (!other.m_submode) {
+            return std::strong_ordering::greater;
+        }
+        return (*m_submode) <=> (*other.m_submode);
+    }
+    return modeCmp;
+}
+
+template <typename T>
+std::optional<Siri::Mode::Submode> Siri::Mode::submodeEnumKeyLookup(QStringView key)
+{
+    auto me = QMetaEnum::fromType<T>();
+    bool ok = false;
+    const auto m = static_cast<T>(me.keyToValue(key.toUtf8().constData(), &ok));
+    if (ok) {
+        return m;
+    }
+    return {};
+}
+
+template <typename T>
+void Siri::Mode::fromModeSubmodeImpl(Line::Mode mode, std::vector<Mode> &result)
+{
+    if constexpr (requires(T) { mode_map<T>[0]; }) {
+        for (const auto &m : mode_map<T>) {
+            if (m.mode == mode) {
+                Mode res(Siri::Mode::VehicleMode::unknown);;
+                if constexpr (std::is_same_v<T, Siri::Mode::VehicleMode>) {
+                    res = Mode(m.siriMode);
+                } else {
+                    res = Mode(modeForSubmode(m.siriMode), m.siriMode);
+                }
+                auto it = std::lower_bound(result.begin(), result.end(), res);
+                // this is sufficient as we process submodes last
+                if (it != result.end() && ((*it) == res)) {
+                    continue;
+                }
+                if (it != result.begin() && ((*std::prev(it)).contains(res))) {
+                    continue;
+                }
+                result.insert(it, res);
+            }
+        }
+    }
+}
+
+bool Siri::Mode::contains(const Siri::Mode &other) const
+{
+    return m_mode == other.m_mode && (!m_submode.has_value() || (other.m_submode.has_value() && m_submode == other.m_submode));
+}
+
+const char* Siri::Mode::modeString() const
+{
+    return toString(m_mode);
+}
+
+const char* Siri::Mode::submodeString() const
+{
+    if (m_submode) {
+        return std::visit([](auto &&m) { return Siri::Mode::toString(m); }, *m_submode);
+    }
+    return nullptr;
+}
+
+const char* Siri::Mode::submodeTypeName() const
+{
+    if (m_submode) {
+        return std::visit([](auto &&m) { return QMetaEnum::fromType<std::decay_t<decltype(m)>>().name(); }, *m_submode);
+    }
+    return nullptr;
+}
+
+Siri::Mode Siri::Mode::fromJson(const QJsonObject &json)
+{
+    return Siri::Mode::fromString(json.value("mode"_L1).toString(), json.value("submode"_L1).toString());
+}
+
+std::vector<Siri::Mode> Siri::Mode::fromJson(const QJsonArray &json)
+{
+    auto res = Json::fromJson<Siri::Mode>(json);
+    std::sort(res.begin(), res.end());
+    return res;
+}
+
+std::vector<Siri::Mode> Siri::Mode::intersection(const std::vector<Siri::Mode> &modes1, const std::vector<Siri::Mode> &modes2)
+{
+    assert(std::ranges::is_sorted(modes1));
+    assert(std::ranges::is_sorted(modes2));
+
+    std::vector<Siri::Mode> res;
+    for (auto it1 = modes1.begin(), it2 = modes2.begin(); it1 != modes1.end() && it2 != modes2.end();) {
+        const auto cmp = (*it1) <=> (*it2);
+        if (cmp == std::strong_ordering::equal) {
+            res.push_back(*it1);
+            ++it1;
+            ++it2;
+            continue;
+        }
+        if (cmp == std::strong_ordering::less) {
+            if (!(*it1).hasSubmode() && (*it1).mode() == (*it2).mode()) {
+                res.push_back(*it2),
+                ++it2;
+            } else {
+                ++it1;
+            }
+            continue;
+        }
+        if (cmp == std::strong_ordering::greater) {
+            if (!(*it2).hasSubmode() && (*it1).mode() == (*it2).mode()) {
+                res.push_back(*it1),
+                ++it1;
+            } else {
+                ++it2;
+            }
+            continue;
+        }
+    }
+    return res;
+}
+
+#include "moc_mode.cpp"
---
+++ src/lib/siri/mode.h
@@ -0,0 +1,302 @@
+/*
+    SPDX-FileCopyrightText: 2026 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#ifndef KPUBLICTRANSPORT_SIRI_MODE_H
+#define KPUBLICTRANSPORT_SIRI_MODE_H
+
+#include <KPublicTransport/Line>
+
+#include <internal/instance_counter.h>
+
+#include <QMetaEnum>
+
+class QJsonArray;
+class QJsonObject;
+
+namespace KPublicTransport {
+namespace Siri {
+
+#define SIRI_SUBMODE_ENUM(Type, ModeValue) \
+Q_ENUM(Type) \
+private: \
+    static constexpr auto _submode_counter(detail::num<decltype(_submode_counter(detail::num<>()))::value + 1> n) \
+        -> decltype(n) { return {}; } \
+    static std::optional<Submode> submodeFromString(decltype(_submode_counter(detail::num<>())) n, QStringView s, VehicleMode mode) \
+    { \
+        if (mode != VehicleMode::ModeValue ) { \
+            if (auto sm = submodeFromString(n.prev(), s, mode); sm) { return sm; } \
+        }\
+        if (mode == VehicleMode::ModeValue || mode == VehicleMode::unknown) { \
+            return submodeEnumKeyLookup<Type>(s); \
+        } \
+        return {}; \
+    } \
+    static constexpr VehicleMode modeForSubmode(Type) { return VehicleMode::ModeValue; } \
+    static void fromModeSubmode(decltype(_submode_counter(detail::num<>())) n, Line::Mode mode, std::vector<Mode> &result) \
+    { \
+        fromModeSubmode(n.prev(), mode, result); \
+        fromModeSubmodeImpl<Type>(mode, result); \
+    } \
+
+/** SIRI mode of transport enums.
+ *
+ *  @note The enums here also act as string tables, so their exact spelling matters
+ *  and has to match the SIRI standard.
+ */
+class Mode {
+    Q_GADGET
+public:
+    enum class VehicleMode {
+        air,
+        bus,
+        cableway,
+        water,
+        funicular,
+        lift,
+        rail,
+        snowAndIce,
+        metro,
+        taxi,
+        tram,
+        trolleybus,
+        monorail,
+        coach,
+        carpool,
+        other,
+        unknown,
+    };
+    Q_ENUM(VehicleMode)
+
+    enum class AirSubmode {
+        internationalFlight,
+        domesticFlight,
+        intercontinentalFlight,
+        domesticScheduledFlight,
+        shuttleFlight,
+        intercontinentalCharterFlight,
+        internationalCharterFlight,
+        roundTripCharterFlight,
+        sightseeingFlight,
+        helicopterService,
+        domesticCharterFlight,
+        SchengenAreaFlight,
+        airshipService,
+        shortHaulInternationalFlight,
+    };
+
+    enum class BusSubmode {
+        localBus,
+        regionalBus,
+        expressBus,
+        nightBus,
+        postBus,
+        specialNeedsBus,
+        mobilityBus,
+        mobilityBusForRegisteredDisabled,
+        sightseeingBus,
+        shuttleBus,
+        highFrequencyBus,
+        dedicatedLaneBus,
+        schoolBus,
+        schoolAndPublicServiceBus,
+        railReplacementBus,
+        demandAndResponseBus,
+        airportLinkBus,
+    };
+
+    enum class CoachSubmode {
+        internationalCoach,
+        nationalCoach,
+        shuttleCoach,
+        regionalCoach,
+        specialCoach,
+        schoolCoach,
+        sightseeingCoach,
+        touristCoach,
+        commuterCoach,
+    };
+
+    enum class MetroSubmode {
+        metro,
+        tube,
+        urbanRailway,
+    };
+
+    enum class RailSubmode {
+        local,
+        highSpeedRail,
+        suburbanRailway,
+        regionalRail,
+        interregionalRail,
+        longDistance,
+        international,
+        sleeperRailService,
+        nightRail,
+        carTransportRailService,
+        touristRailway,
+        airportLinkRail,
+        railShuttle,
+        replacementRailService,
+        specialTrain,
+        crossCountryRail,
+        rackAndPinionRailway,
+    };
+
+    enum class TramSubmode {
+        cityTram,
+        localTram,
+        regionalTram,
+        sightseeingTram,
+        shuttleTram,
+        trainTram,
+    };
+
+    enum class WaterSubmode {
+        internationalCarFerry,
+        nationalCarFerry,
+        regionalCarFerry,
+        localCarFerry,
+        internationalPassengerFerry,
+        nationalPassengerFerry,
+        regionalPassengerFerry,
+        localPassengerFerry,
+        postBoat,
+        trainFerry,
+        roadFerryLink,
+        airportBoatLink,
+        highSpeedVehicleService,
+        highSpeedPassengerService,
+        sightseeingService,
+        schoolBoat,
+        cableFerry,
+        riverBus,
+        scheduledFerry,
+        shuttleFerryService,
+        canalBarge,
+    };
+
+    enum class TelecabinSubmode {
+        telecabin,
+        cableCar,
+        lift,
+        chairLift,
+        dragLift,
+        telecabinLink,
+    };
+
+    enum class FunicularSubmode {
+        funicular,
+        streetCableCar,
+    };
+
+    enum class TaxiSubmode {
+        communalTaxi,
+        charterTaxi,
+        waterTaxi,
+        railTaxi,
+        bikeTaxi,
+        blackCab,
+        miniCab,
+    };
+
+    using Submode = std::variant<
+        AirSubmode,
+        BusSubmode,
+        CoachSubmode,
+        MetroSubmode,
+        RailSubmode,
+        TramSubmode,
+        WaterSubmode,
+        TelecabinSubmode,
+        FunicularSubmode,
+        TaxiSubmode
+    >;
+
+    Mode(VehicleMode mode, std::optional<Submode> submode = {})
+        : m_mode(mode)
+        , m_submode(submode)
+    {
+    }
+
+    /** Create a SIRI mode from its string representation.
+     *  This also works with either one of the arguments missing.
+     */
+    [[nodiscard]] static Mode fromString(QStringView mode, QStringView subMode);
+
+    /** Convert a SIRI mode enum to its string representation. */
+    template <typename T>
+    [[nodiscard]] static const char* toString(T mode);
+
+    /** Returns the set of SIRI (sub)modes that match one of our modes. */
+    [[nodiscard]] static std::vector<Mode> fromMode(Line::Mode);
+    [[nodiscard]] static std::vector<Mode> fromModes(const std::vector<Line::Mode> &modes);
+
+    [[nodiscard]] VehicleMode mode() const { return m_mode; }
+    [[nodiscard]] bool hasSubmode() const { return m_submode.has_value(); }
+    [[nodiscard]] std::optional<Submode> submode() const { return m_submode; }
+
+    /** SIRI string representation of the mode. */
+    [[nodiscard]] const char* modeString() const;
+    [[nodiscard]] const char* submodeString() const;
+    /** SIRI type name of the submode in use. */
+    [[nodiscard]] const char* submodeTypeName() const;
+
+    /** Convert SIRI mode to our mode enum. */
+    [[nodiscard]] Line::Mode toMode() const;
+
+    std::strong_ordering operator<=>(const Mode &other) const;
+    bool operator==(const Mode &other) const { return operator<=>(other) == std::strong_ordering::equal; }
+
+    /** Returns @c true if this mode is a less specific parent of @p other. */
+    [[nodiscard]] bool contains(const Mode &other) const;
+
+    /** Loads SIRI modes from a JSON object.
+     *  For use in e.g. OJP backend configuration.
+     */
+    [[nodiscard]] static Mode fromJson(const QJsonObject &json);
+    [[nodiscard]] static std::vector<Mode> fromJson(const QJsonArray &json);
+
+    /** Compute the intersection of two mode sets.
+     *  @note both inputs are assumed to be ordered.
+     */
+    [[nodiscard]] static std::vector<Mode> intersection(const std::vector<Mode> &modes1, const std::vector<Mode> &modes2);
+
+private:
+    template <typename T>
+    [[nodiscard]] static std::optional<Submode> submodeEnumKeyLookup(QStringView key);
+    template <typename T>
+    static void fromModeSubmodeImpl(Line::Mode mode, std::vector<Mode> &result);
+
+    static constexpr detail::num<0> _submode_counter(detail::num<0>) { return {}; }
+    static constexpr std::optional<Submode> submodeFromString(detail::num<0>, QStringView, VehicleMode) { return {}; }
+    static constexpr void fromModeSubmode(detail::num<0>, Line::Mode, std::vector<Mode>&) {};
+
+    SIRI_SUBMODE_ENUM(AirSubmode, air)
+    SIRI_SUBMODE_ENUM(BusSubmode, bus)
+    SIRI_SUBMODE_ENUM(CoachSubmode, coach)
+    SIRI_SUBMODE_ENUM(MetroSubmode, metro)
+    SIRI_SUBMODE_ENUM(RailSubmode, rail)
+    SIRI_SUBMODE_ENUM(TramSubmode, tram)
+    SIRI_SUBMODE_ENUM(WaterSubmode, water)
+    SIRI_SUBMODE_ENUM(TelecabinSubmode, cableway)
+    SIRI_SUBMODE_ENUM(FunicularSubmode, funicular)
+    SIRI_SUBMODE_ENUM(TaxiSubmode, taxi)
+
+    VehicleMode m_mode = VehicleMode::unknown;
+    std::optional<Submode> m_submode;
+};
+
+#undef SIRI_SUBMODE_ENUM
+
+template <typename T>
+const char* Mode::toString(T mode)
+{
+    return QMetaEnum::fromType<T>().valueToKey(std::to_underlying(mode));
+}
+
+}
+}
+
+#endif
