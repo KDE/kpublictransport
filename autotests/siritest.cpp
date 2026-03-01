bf931de7704ac9c71d73eedc8a74246e79e4c8de
Volker Krause <vkrause@kde.org>
2026-03-01 13:45:00 +0100

    Add infrastructure for dealing with SIRI mode types
    This will replace the partial solution we have for this in
    the OTP and OJP backends, and allow proper reverse mapping
    based on the specific capabilities of the backend.
    
    It's build in a way that everything is derived from the enum
    declarations and the mapping tables to our mode types,
    without having to maunally maintain string tables or
    other auxiliary mappings.

--- autotests/siritest.cpp
+++ autotests/siritest.cpp
@@ -4,6 +4,7 @@
 */
 
 #include <siri/duration.cpp>
+#include <siri/mode.h>
 
 #include <QTest>
 
@@ -33,6 +34,72 @@ private Q_SLOTS:
         QFETCH(QString, duration);
         QFETCH(int, seconds);
         QCOMPARE(Siri::fromDuration(duration), std::chrono::seconds(seconds));
+    }
+
+    void testMode()
+    {
+        QCOMPARE(Siri::Mode::fromString(u"rail", {}).toMode(), Line::Train);
+        QCOMPARE(Siri::Mode::fromString(u"rail", u"highSpeedRail").toMode(), Line::LongDistanceTrain);
+        QCOMPARE(Siri::Mode::fromString({}, u"local").toMode(), Line::LocalTrain);
+        QCOMPARE(Siri::Mode::fromString({}, u"localBus").toMode(), Line::Bus);
+        QCOMPARE(Siri::Mode::fromString(u"foo", u"bar").toMode(), Line::Unknown);
+        QCOMPARE(Siri::Mode::fromString(u"metro", u"newMetroServer").toMode(), Line::Metro);
+
+        QCOMPARE(Siri::Mode::toString(Siri::Mode::VehicleMode::bus), "bus");
+        QCOMPARE(Siri::Mode::toString(Siri::Mode::CoachSubmode::internationalCoach), "internationalCoach");
+
+        auto m = Siri::Mode::fromMode(Line::Tramway);
+        QCOMPARE(m.size(), 1);
+        QCOMPARE(m[0].mode(), Siri::Mode::VehicleMode::tram);
+        QVERIFY(!m[0].hasSubmode());
+
+        m = Siri::Mode::fromMode(Line::Bus);
+        QCOMPARE(m.size(), 2);
+        QCOMPARE(m[0].mode(), Siri::Mode::VehicleMode::bus);
+        QCOMPARE(m[1].mode(), Siri::Mode::VehicleMode::trolleybus);
+
+        m = Siri::Mode::fromMode(Line::LongDistanceTrain);
+        QCOMPARE(m.size(), 5);
+        QCOMPARE(m[0].mode(), Siri::Mode::VehicleMode::rail);
+        QCOMPARE(m[0].hasSubmode(), true);
+        QCOMPARE(std::get<Siri::Mode::RailSubmode>(*m[0].submode()), Siri::Mode::RailSubmode::highSpeedRail);
+        QCOMPARE(m[1].mode(), Siri::Mode::VehicleMode::rail);
+        QCOMPARE(std::get<Siri::Mode::RailSubmode>(*m[1].submode()), Siri::Mode::RailSubmode::longDistance);
+        QCOMPARE(m[1].hasSubmode(), true);
+
+        const Siri::Mode hsr{Siri::Mode::VehicleMode::rail, Siri::Mode::RailSubmode::highSpeedRail};
+        const Siri::Mode ldr{Siri::Mode::VehicleMode::rail, Siri::Mode::RailSubmode::longDistance};
+        QCOMPARE_EQ(hsr, hsr);
+        QCOMPARE_LT(hsr, ldr);
+        QCOMPARE_GT(ldr, hsr);
+        QCOMPARE_NE(ldr, hsr);
+        const Siri::Mode rm{Siri::Mode::VehicleMode::rail};
+        QCOMPARE_NE(rm, hsr);
+        QCOMPARE_LT(rm, hsr);
+
+        auto modes = Siri::Mode::fromModes({Line::LongDistanceTrain, Line::LocalTrain, Line::Train});
+        QCOMPARE(modes.size(), 2);
+        QCOMPARE(modes[0].mode(), Siri::Mode::VehicleMode::rail);
+        QCOMPARE(modes[0].hasSubmode(), false);
+        QCOMPARE(modes[1].mode(), Siri::Mode::VehicleMode::monorail);
+        QCOMPARE(modes[1].hasSubmode(), false);
+
+        auto modes1 = Siri::Mode::fromModes({Line::LongDistanceTrain, Line::LocalTrain});
+        auto modes2 = Siri::Mode::fromModes({Line::LocalTrain});
+        QCOMPARE(modes2.size(), 2); // local, regional
+        modes = Siri::Mode::intersection(modes1, modes2);
+        QCOMPARE(modes.size(), 2);
+        QCOMPARE(modes[0].hasSubmode(), true);
+        QCOMPARE(std::get<Siri::Mode::RailSubmode>(*modes[0].submode()), Siri::Mode::RailSubmode::local);
+        auto modesReverse = Siri::Mode::intersection(modes2, modes1);
+        QCOMPARE(modes, modesReverse);
+
+        modes1 = {Siri::Mode(Siri::Mode::VehicleMode::rail)};
+        modes2 = {Siri::Mode(Siri::Mode::VehicleMode::rail, Siri::Mode::RailSubmode::local)};
+        modes = Siri::Mode::intersection(modes1, modes2);
+        QCOMPARE(modes.size(), 1);
+        QCOMPARE(modes[0].hasSubmode(), true);
+        QCOMPARE(std::get<Siri::Mode::RailSubmode>(*modes[0].submode()), Siri::Mode::RailSubmode::local);
     }
 };
 
