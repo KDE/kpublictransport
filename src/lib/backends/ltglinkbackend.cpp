2c2363f1590e3af75a9e6f0f3912b9e68f9569e4
Jonah Brüchert <jbb@kaidan.im>
2026-06-02 21:53:35 +0200

    ltglink: Use corect date for booking link

--- src/lib/backends/ltglinkbackend.cpp
+++ src/lib/backends/ltglinkbackend.cpp
@@ -204,7 +204,8 @@ bool LTGLinkBackend::queryJourney(const JourneyRequest &req, JourneyReply *reply
                 QUrlQuery query;
                 query.addQueryItem(u"oStop"_s, req.from().identifier(LTGLINK_IDENTIFIER));
                 query.addQueryItem(u"dStop"_s, req.to().identifier(LTGLINK_IDENTIFIER));
-                query.addQueryItem(u"oDate"_s, date.toString(Qt::ISODate));
+                query.addQueryItem(u"oDate"_s,
+                                   journey.expectedDepartureTime().date().toString(Qt::ISODate));
                 query.addQueryItem(u"fareClasses"_s, u"BONUS_SCHEME_GROUP.ADULT,1"_s);
                 bookingUrl.setQuery(query);
                 journey.setBookingUrl(bookingUrl);
